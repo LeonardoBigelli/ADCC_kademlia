e52747c606fecc0b43dc57aa0a565f4f5afcc1b4
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2024-12-18 10:19:04 +0100

    Inizio scrittura nodo
    Prima scrittura del file, utilizzando il gen_server.

--- nodo.erl
+++ nodo.erl
@@ -1,4 +1,17 @@
 % interfaccia per gestire i nodi kademlia
 
 -module(node).
--export([]).
+-behaviour(gen_server).
+
+-export([init/1, start_link/2]).
+
+% un nodo kademlia ha un proprio stato,
+% ovvero un insieme di informazioni,
+% quali id, k_buckets, storage, timer
+-record(state, {id, k_buckets, storage, timer}).
+
+% inizializzazione
+init(Id) -> {ok, #state{id = Id, k_buckets = vuoto, storage = vuoto, timer = 0}}.
+
+% avvio del nodo (CAPIRE DOVE USARE LA SPAWN)
+start_link(Id, Options) -> gen_server:start_link({local, Id}, ?MODULE, Id, Options).

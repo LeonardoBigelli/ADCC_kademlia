653508c845aaa0389f1bbd859cbb88ee017f30db
LeonardoBigelli <l.bigelli2@campus.uniurb.it>
2025-01-04 17:11:00 +0100

    Aggiunta funzionalità dei K_buckets
    Ora quando un nodo nuovo entra nella rete kademlia gli viene fornita una prima lista di (=<4) nodi vicini ad esso.

--- nodo.erl
+++ nodo.erl
@@ -3,7 +3,7 @@
 -module(nodo).
 -include_lib("stdlib/include/qlc.hrl").
 
--export([start_system/1, new_erlang_node/1, node_behavior/1]).
+-export([start_system/1, new_erlang_node/1, node_behavior/1, print_all/0]).
 % gestione dei record per la tebella di bostrap
 -record(bootstrap_table, {id, pid, last_ping}).
 
@@ -77,14 +77,63 @@ bootstrap_node_loop(Id) ->
                 {_, ok} ->
                     io:format("[[--BOOSTRAP--]] Nodo aggiunto con successo: ~p~n", [NodeId]),
                     % assegnazione dell'Id e della K_buckets al nodo che entra
-                    Buckets = [],
+                    Buckets = get_4_buckets(NodeId),
                     From ! {refresh, {NodeId, Buckets}};
                 {_, Reason} ->
                     io:format("[[--BOOSTRAP--]] Errore durante l'aggiunta del nodo: ~p~n", [Reason])
             end,
+            bootstrap_node_loop(Id);
+        % stampa di tutta la sua tabella di mnesia
+        {print} ->
+            print_all(),
             bootstrap_node_loop(Id);
         % messaggio generico
         _ ->
             io:format("[[--BOOSTRAP--]] --> Messaggio sconosciuto."),
             bootstrap_node_loop(Id)
     end.
+
+% funzione invocata per visualizzare il contenuto dello schema di mnesia,
+% solo il nodo Bootstrap può invocarla
+print_all() ->
+    Print = fun(#bootstrap_table{id = Id, pid = Pid, last_ping = L}, Acc) ->
+        Acc ++ [{Id, Pid, L}]
+    end,
+    Tran = fun() -> mnesia:foldr(Print, [], bootstrap_table) end,
+    {_, Res} = mnesia:transaction(Tran),
+    lists:foreach(
+        fun({Id, Pid, L}) ->
+            io:format(" id:  ~p , pid:  ~p, last ping:  ~p \n", [Id, Pid, L])
+        end,
+        Res
+    ).
+
+% funzione invocata per fornire ad un nodo, la sua lista k_buckets
+get_4_buckets(NodeId) ->
+    Tran = fun() ->
+        % Ottieni tutti i record nella tabella
+        mnesia:foldr(
+            fun(#bootstrap_table{id = Id, pid = Pid, last_ping = L}, Acc) ->
+                % Escludi il nodo chiamante
+                case Id == NodeId of
+                    % Non aggiungere il nodo chiamante
+                    true ->
+                        Acc;
+                    false ->
+                        % Calcola la distanza XOR e aggiungi alla lista
+                        Distance = NodeId bxor Id,
+                        Acc ++ [{Distance, Id, Pid, L}]
+                end
+            end,
+            [],
+            bootstrap_table
+        )
+    end,
+    % Esegui la transazione
+    {atomic, AllRecords} = mnesia:transaction(Tran),
+    % Ordina per distanza XOR
+    SortedRecords = lists:sort(fun({D1, _, _, _}, {D2, _, _, _}) -> D1 < D2 end, AllRecords),
+    % Prendi i primi 4
+    Top4 = lists:sublist(SortedRecords, 4),
+    % Restituisci la lista
+    Top4.
